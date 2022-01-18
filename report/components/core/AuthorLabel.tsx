860930def383bb4b5a2cd1ee9440e31fac3aa850
Martín Lombardo <to@mlomb.me>
2022-01-18 20:32:31 -0300

    padding in author discriminator

--- report/components/core/AuthorLabel.tsx
+++ report/components/core/AuthorLabel.tsx
@@ -96,7 +96,7 @@ const AuthorLabel = ({ index }: Props) => {
         <div className="Label" title={author.n}>
             <div className="Label__avatar">{avatar}</div>
             <span className="Label__name">{author.n}</span>
-            {author.d !== undefined && <span className="Label__discriminator">#{author.d}</span>}
+            {author.d !== undefined && <span className="Label__discriminator">#{`${author.d}`.padStart(4, "0")}</span>}
         </div>
     );
 };
