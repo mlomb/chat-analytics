e51029d6200f2eaee99ed403eb9630e615c19a43
hopperelec <hopperelec@gmail.com>
2023-01-09 17:50:08 +0000

    Regex toggle for MostUsedWords (#32)
    * Added a regex toggle to the MostUsedWords card which treats the filter as a regular expression
    Closes issue https://github.com/mlomb/chat-analytics/issues/30
    
    Co-authored-by: Martín Lombardo <to@mlomb.me>

--- report/components/core/SearchInput.tsx
+++ report/components/core/SearchInput.tsx
@@ -1,22 +1,83 @@
+import { useRef, useState } from "react";
+
 import "@assets/styles/SearchInput.less";
 
+import Tooltip from "@report/components/core/Tooltip";
+
 interface Props {
-    value: string;
-    onChange: (value: string) => void;
     placeholder?: string;
+    allowRegex?: boolean;
+    onChange: (value: string | RegExp) => void;
 }
 
-const SearchInput = ({ value, placeholder, onChange }: Props) => (
-    <div className="SearchInput">
-        <div className="SearchInput__mag"></div>
-        <input
-            className="SearchInput__input"
-            type="text"
-            placeholder={placeholder}
-            value={value}
-            onChange={(e) => onChange(e.target.value)}
-        />
-    </div>
-);
+const SearchInput = ({ onChange, placeholder, allowRegex }: Props) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const [input, setInput] = useState<string>(""); // true text value of input
+    const [error, setError] = useState<boolean>(false);
+    const [regexEnabled, setRegexEnabled] = useState<boolean>(false);
+
+    const onInputChanged = (value: string, _regexEnabled: boolean) => {
+        setInput(value);
+
+        if (allowRegex && _regexEnabled) {
+            // try to parse input as regular expression
+            try {
+                const expr = new RegExp(value, "ui");
+                onChange(expr);
+                setError(false);
+            } catch (e) {
+                // invalid regex
+                // pass never matching regex and show error
+                onChange(/[]/);
+                setError(true);
+            }
+        } else {
+            // pass text directly
+            onChange(value);
+            setError(false);
+        }
+    };
+
+    const onToggleRegex = (enabled: boolean) => {
+        setRegexEnabled(enabled);
+        onInputChanged(input, enabled);
+
+        // always give the focus back to the input
+        inputRef.current?.focus();
+    };
+
+    return (
+        <div className="SearchInput">
+            <div className="SearchInput__mag"></div>
+            <input
+                ref={inputRef}
+                className={[
+                    "SearchInput__input",
+                    allowRegex ? "SearchInput__input--regex" : "",
+                    error ? "SearchInput__input--error" : "",
+                ].join(" ")}
+                type="text"
+                placeholder={placeholder}
+                value={input}
+                onChange={(e) => onInputChanged(e.target.value, regexEnabled)}
+            />
+            {allowRegex && (
+                <Tooltip
+                    content={`Use regular expressions to search`}
+                    children={
+                        <input
+                            className="SearchInput__regex"
+                            type="checkbox"
+                            onMouseDown={(e) => e.preventDefault()}
+                            checked={regexEnabled}
+                            onChange={(e) => onToggleRegex(e.target.checked)}
+                        />
+                    }
+                />
+            )}
+        </div>
+    );
+};
 
 export default SearchInput;
