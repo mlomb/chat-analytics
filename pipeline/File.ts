33a9a03382746651d8c18f3cee9da02d0635f1f3
hopperelec <hopperelec@gmail.com>
2023-01-09 15:59:10 +0000

    Moved File.ts constants outside of functions (#29)
    * Defined File.ts constants `mappings` (now `ATTACHMENT_EXTS`) and `docMimeTypes` (now `DOC_MIME_TYPES`) outside of functions
    
    * formatting
    
    Co-authored-by: Martín Lombardo <to@mlomb.me>

--- pipeline/File.ts
+++ pipeline/File.ts
@@ -46,24 +46,36 @@ export function downloadFile(filepath: any, responseType: XMLHttpRequestResponse
     });
 }
 
+const ATTACHMENT_EXTS: {
+    [key in AttachmentType]?: string[];
+} = {
+    [AttachmentType.Image]: ["png", "jpg", "jpeg", "webp", "bmp", "tiff", "tif", "svg", "ico", "psd"],
+    [AttachmentType.ImageAnimated]: ["gif", "gifv", "apng"],
+    [AttachmentType.Video]: ["mp4", "webm", "mkv", "flv", "mov", "avi", "wmv", "mpg", "mpeg", "avi"],
+    [AttachmentType.Audio]: ["mp3", "ogg", "wav", "flac", "m4a"],
+    [AttachmentType.Document]: ["doc", "docx", "odt", "pdf", "xls", "xlsx", "ods", "ppt", "pptx", "txt", "html"],
+};
 export const getAttachmentTypeFromFileName = (filename: string): AttachmentType => {
     const ext = (filename.split(".").pop() || "").toLocaleLowerCase();
-    const mappings: {
-        [key in AttachmentType]?: string[];
-    } = {
-        [AttachmentType.Image]: ["png", "jpg", "jpeg", "webp", "bmp", "tiff", "tif", "svg", "ico", "psd"],
-        [AttachmentType.ImageAnimated]: ["gif", "gifv", "apng"],
-        [AttachmentType.Video]: ["mp4", "webm", "mkv", "flv", "mov", "avi", "wmv", "mpg", "mpeg", "avi"],
-        [AttachmentType.Audio]: ["mp3", "ogg", "wav", "flac", "m4a"],
-        [AttachmentType.Document]: ["doc", "docx", "odt", "pdf", "xls", "xlsx", "ods", "ppt", "pptx", "txt", "html"],
-    };
     for (let type: AttachmentType = 0; type <= AttachmentType.Other; type++) {
-        if (mappings[type]?.includes(ext)) return type;
+        if (ATTACHMENT_EXTS[type]?.includes(ext)) return type;
     }
     // unknown or generic
     return AttachmentType.Other;
 };
 
+const DOC_MIME_TYPES: string[] = [
+    "application/pdf",
+    "application/epub",
+    "application/epub+zip",
+    "text/html",
+    "application/rtf",
+    "application/msword",
+    "application/vnd.oasis.opendocument.spreadsheet",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    "text/plain",
+];
 export const getAttachmentTypeFromMimeType = (mimeType: string): AttachmentType => {
     mimeType = mimeType.toLocaleLowerCase();
 
@@ -71,20 +83,7 @@ export const getAttachmentTypeFromMimeType = (mimeType: string): AttachmentType 
     if (mimeType.startsWith("image/")) return AttachmentType.Image;
     if (mimeType.startsWith("video/")) return AttachmentType.Video;
     if (mimeType.startsWith("audio/")) return AttachmentType.Audio;
-
-    const docMimeTypes = [
-        "application/pdf",
-        "application/epub",
-        "application/epub+zip",
-        "text/html",
-        "application/rtf",
-        "application/msword",
-        "application/vnd.oasis.opendocument.spreadsheet",
-        "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-        "text/plain",
-    ];
-    if (docMimeTypes.includes(mimeType)) return AttachmentType.Document;
+    if (DOC_MIME_TYPES.includes(mimeType)) return AttachmentType.Document;
 
     // console.log(`Unknown mime type: ${mimeType}`);
 
