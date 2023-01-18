e74ff6acb73fd1e7fe359d1df3dbc84f9974a251
Martín Lombardo <to@mlomb.me>
2023-01-18 14:02:26 -0300

    Messenger support for groups and title

--- pipeline/parse/parsers/MessengerParser.ts
+++ pipeline/parse/parsers/MessengerParser.ts
@@ -30,13 +30,13 @@ export class MessengerParser extends Parser {
         // so we can just parse the whole file at once :)
         const fileContent = JSON.parse(textContent) as MessengerExportFile;
 
-        const guildIndex = this.builder.addGuild(fileContent.thread_path, {
-            name: fileContent.title,
+        const guildIndex = this.builder.addGuild("Default", {
+            name: "Facebook Chats",
         });
         const assignedChannelIndex = this.builder.addChannel(fileContent.thread_path, {
             name: fileContent.title,
             guildIndex: guildIndex,
-            type: "group", // TODO!
+            type: fileContent.participants.length > 2 ? "group" : "dm",
         });
 
         // we iterate the messages in reverse order since we want to iterate from older to newer
