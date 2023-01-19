bf9610c9fab93398d2c6e097e4527c0337f6f6c0
Martín Lombardo <to@mlomb.me>
2023-01-19 20:15:38 -0300

    fastText compilation with shell environment
    what a painful commit

--- pipeline/process/FastTextModel.ts
+++ pipeline/process/FastTextModel.ts
@@ -1,8 +1,23 @@
 import { progress } from "@pipeline/Progress";
 import { downloadFile } from "@pipeline/File";
 
+interface FastTextModelClass {
+    loadModel: (path: string) => void;
+    predict: (text: string, k: number, threshold: number) => any;
+}
+
+interface FastTextModule {
+    FS: {
+        open: (path: string, flags: string) => number;
+        write: (stream: number, buffer: Uint8Array, offset: number, length: number, position: number) => number;
+        close: (stream: number) => void;
+    };
+
+    FastText: new () => FastTextModelClass;
+}
+
 export class FastTextModel {
-    constructor(private readonly model: any) {}
+    constructor(private readonly model: FastTextModelClass) {}
 
     predict(text: string, k = 1, threshold = 0.0): [number, string][] {
         const predictions = this.model.predict(text, k, threshold);
@@ -12,20 +27,23 @@ export class FastTextModel {
         return res;
     }
 }
-
 export const loadFastTextModel = async (modelName: string) => {
-    progress.new("Downloading FastText WASM module");
-    // @ts-ignore
-    const { default: fastTextModularized } = await import("@assets/fasttext/fasttext_wasm.js");
-    const fastTextModule = await fastTextModularized();
+    const fastTextModuleJs = await downloadFile(`/fasttext/fasttext_wasm.js`, "text");
+    const fastTextModuleFn = new Function(fastTextModuleJs + `; return Module;`)();
+    const fastTextModule = (await fastTextModuleFn({
+        wasmBinary: await downloadFile(`/fasttext/fasttext_wasm.wasm`, "arraybuffer"),
+    })) as FastTextModule;
+
+    const model = new Uint8Array(await downloadFile(`/data/models/${modelName}.ftz`, "arraybuffer"));
+
+    // NOTE: writeFile is not available since the closure compiler optimized it out (but we still have open, write and close)
+    // we could configure CC to keep it but it's not worth it, this works
+    const { FS } = fastTextModule;
+    const stream = FS.open("model.bin", "w+");
+    FS.write(stream, model, 0, model.length, 0);
+    FS.close(stream);
+
     const fastText = new fastTextModule.FastText();
-    progress.done();
-
-    const model = new Uint8Array(await downloadFile(`/data/${modelName}.ftz`, "arraybuffer"));
-
-    progress.new("Loading model", modelName);
-    const FS = fastTextModule.FS;
-    await FS.writeFile("model.bin", model);
     fastText.loadModel("model.bin");
     progress.done();
 
