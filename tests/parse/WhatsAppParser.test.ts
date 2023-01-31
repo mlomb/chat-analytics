46cb55b7562e5619fd4bb22d909dd45f2d58dd70
Martín Lombardo <to@mlomb.me>
2023-01-31 13:36:56 -0300

    added group sample whatsapp

--- tests/parse/WhatsAppParser.test.ts
+++ tests/parse/WhatsAppParser.test.ts
@@ -2,7 +2,7 @@ import { WhatsAppParser } from "@pipeline/parse/parsers/WhatsAppParser";
 
 import { runParserFromSamples } from "./Util";
 
-it("sample1 should resolve correctly", async () => {
+test("1A_3M.txt should resolve correctly", async () => {
     const parsed = await runParserFromSamples(WhatsAppParser, ["whatsapp/1A_3M.txt"]);
 
     // guilds
@@ -55,4 +55,55 @@ it("sample1 should resolve correctly", async () => {
             timestamp: new Date(2021, 0, 1, 12, 2, 0).getTime(),
         }),
     ]);
+});
+
+test("4A_11M.txt should resolve correctly", async () => {
+    const parsed = await runParserFromSamples(WhatsAppParser, ["whatsapp/4A_11M.txt"]);
+
+    // guilds
+    expect(parsed.guilds).toHaveLength(1);
+    expect(parsed.guilds[0]).toHaveProperty("name", "WhatsApp Chats");
+
+    // channels
+    expect(parsed.channels).toEqual(
+        expect.arrayContaining([
+            expect.objectContaining({
+                guildId: 0,
+                type: "group",
+            }),
+            expect.not.objectContaining({
+                type: expect.not.stringMatching("group"),
+            }),
+        ])
+    );
+
+    // authors
+    expect(parsed.authors).toEqual(
+        expect.arrayContaining(
+            ["Alice", "Bob", "Eve", "Mallory", "Trent"].map((name) =>
+                expect.objectContaining({
+                    name,
+                    bot: false,
+                })
+            )
+        )
+    );
+
+    // messages
+    expect(parsed.messages).toEqual(
+        expect.arrayContaining([
+            expect.objectContaining({
+                channelId: 0,
+                authorId: "Alice",
+                textContent: "boiii",
+                timestamp: new Date(2020, 8, 12, 23, 55, 0).getTime(),
+            }),
+            expect.objectContaining({
+                channelId: 0,
+                authorId: "Trent",
+                textContent: "👩🏻‍⚖️",
+                timestamp: new Date(2020, 8, 12, 23, 55, 0).getTime(),
+            }),
+        ])
+    );
 });
