587343a069acbecc6461f7816289b75e505a7b14
Martín Lombardo <to@mlomb.me>
2023-02-02 21:02:48 -0300

    changed `reindex` and `remap` in favor of `rank` and `remap` in Util

---
+++ tests/process/Util.test.ts
@@ -0,0 +1,32 @@
+import { rank, remap } from "@pipeline/process/Util";
+
+test.each([
+    [
+        [3, 2, 1],
+        [0, 1, 2],
+    ],
+    [
+        [1, 2, 3],
+        [2, 1, 0],
+    ],
+    [
+        [5, 4, 1, 6, 3, 2],
+        [1, 2, 5, 0, 3, 4],
+    ],
+])("`rank` sorts as expected %p → %p", (input, expected) => {
+    expect(rank(input)).toEqual(expected);
+});
+
+test.each([
+    [
+        [1, 2, 3],
+        [30, 20, 10],
+    ],
+    [
+        [5, 4, 1, 6, 3, 2],
+        [60, 50, 40, 30, 20, 10],
+    ],
+])("`remap` works as expected %p → %p", (input, expected) => {
+    // using rank it ends up being the same as a sort
+    expect(remap((v) => v * 10, input, rank(input))).toEqual(expected);
+});
