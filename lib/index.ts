ddb1f56adb39d71355778445b3044915c76cb1ac
Martín Lombardo <to@mlomb.me>
2023-02-04 00:41:11 -0300

    moved convenience exports into lib index

--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,6 @@
 export * from "@pipeline/index";
+export { Env } from "@pipeline/Env";
+export { Config } from "@pipeline/Types";
+export { FileInput } from "@pipeline/parse/File";
+export { Database } from "@pipeline/process/Types";
+export { Platform } from "@pipeline/Platforms";
